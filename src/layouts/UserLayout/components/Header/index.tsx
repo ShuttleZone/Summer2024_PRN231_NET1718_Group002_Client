697f2219fb3ea12d9163c3e923c8326ba73b271b
duykasama <duykasama@gmail.com>
2024-05-21 20:42:14 +0700

    [ClubsList][Duy] add header

--- src/layouts/UserLayout/components/Header/index.tsx
+++ src/layouts/UserLayout/components/Header/index.tsx
@@ -1,5 +1,22 @@
+import {Button} from "@/components/ui/button";
+import Logo from "./components/Logo";
+import Navbar from "./components/Navbar";
+import {IoPeopleOutline} from "react-icons/io5";
+import {Link} from "react-router-dom";
+
 function Header() {
-    return <header className="bg-blue-500">This is user header</header>;
+    return (
+        <header className="flex justify-between items-center px-6 py-4">
+            <Logo />
+            <Navbar />
+            <Link to="/login">
+                <Button className="bg-[#0a7e53] flex justify-between items-center gap-1">
+                    <IoPeopleOutline size={20} />
+                    <span>Login/Register</span>
+                </Button>
+            </Link>
+        </header>
+    );
 }
 
 export default Header;
