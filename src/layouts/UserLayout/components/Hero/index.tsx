38174d7032205f9d20e48d8fa9cec5a356e236d6
duykasama <duykasama@gmail.com>
2024-05-21 22:31:58 +0700

    [ClubsList][Duy] add hero

---
+++ src/layouts/UserLayout/components/Hero/index.tsx
@@ -0,0 +1,46 @@
+import HeroBackgroundImage from "@/assets/images/breadcrumb-bg2.jpg";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+function Hero() {
+    return (
+        <div className="relative text-white flex flex-col justify-center items-center">
+            <div className="absolute w-full h-full bg-black/60 z-0"></div>
+            <img
+                src={HeroBackgroundImage}
+                alt="hero background"
+                className="w-full h-full object-cover absolute -z-10"
+            />
+            <div className="w-3/4 min-h-40 flex flex-col justify-center items-start">
+                <div className="z-10">
+                    <h1 className="text-3xl font-bold">Clubs List</h1>
+                    <Breadcrumb className="mt-4">
+                        <BreadcrumbList>
+                            <BreadcrumbItem>
+                                <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator />
+                            <BreadcrumbItem className="hover:text-red-500">
+                                <BreadcrumbLink href="/components">
+                                    Components
+                                </BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator />
+                            <BreadcrumbItem>
+                                <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                    </Breadcrumb>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
--- src/layouts/UserLayout/index.tsx
+++ src/layouts/UserLayout/index.tsx
@@ -1,12 +1,14 @@
 import {Outlet} from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 
 function UserLayout() {
     return (
         <div className="h-screen flex flex-col">
             <Header />
-            <main className="flex-grow bg-yellow-500">
+            <main className="flex-grow">
+                <Hero />
                 <Outlet />
             </main>
             <Footer />
