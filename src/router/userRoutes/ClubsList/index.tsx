9867cd59d50bf32f2d81ff463a78078c4d9f7bba
duykasama <duykasama@gmail.com>
2024-05-25 11:41:03 +0700

    [ClubsList][Duy] format time, calculate ratings

--- src/router/userRoutes/ClubsList/index.tsx
+++ src/router/userRoutes/ClubsList/index.tsx
@@ -1,87 +1,42 @@
 import Filter from "./components/Filter";
 import ClubCard from "./components/ClubCard";
-// import {useEffect} from "react";
-// import {useGetClubsQuery} from "@/store/services/clubs/club.api";
+import {useGetClubsQuery} from "@/store/services/clubs/club.api";
 
-interface ClubType {
+export interface ReviewType {
     id: string;
-    name: string;
-    description: string;
-    location: string;
-    image: string;
-    profileImage: string;
-    tags: string[];
-    price: number;
-    reviews: number;
     rating: number;
 }
 
-const clubs: ClubType[] = [
-    {
-        id: "1",
-        name: "Club 1",
-        description: "Club 1 description",
-        location: "Location 1",
-        image: "https://source.unsplash.com/400x400/?club",
-        profileImage: "https://source.unsplash.com/400x400/?profile",
-        tags: ["tag1", "tag2", "tag3"],
-        price: 100,
-        reviews: 100,
-        rating: 4.2,
-    },
-    {
-        id: "2",
-        name: "Club 1",
-        description: "Club 1 description",
-        location: "Location 1",
-        image: "https://source.unsplash.com/400x400/?club",
-        profileImage: "https://source.unsplash.com/400x400/?profile",
-        tags: ["tag1", "tag2", "tag3"],
-        price: 100,
-        reviews: 100,
-        rating: 4,
-    },
-    {
-        id: "3",
-        name: "Club 1",
-        description: "Club 1 description",
-        location: "Location 1",
-        image: "https://source.unsplash.com/400x400/?club",
-        profileImage: "https://source.unsplash.com/400x400/?profile",
-        tags: ["tag1", "tag2", "tag3"],
-        price: 100,
-        reviews: 100,
-        rating: 4,
-    },
-    {
-        id: "4",
-        name: "Club 1",
-        description: "Club 1 description",
-        location: "Location 1",
-        image: "https://source.unsplash.com/400x400/?club",
-        profileImage: "https://source.unsplash.com/400x400/?profile",
-        tags: ["tag1", "tag2", "tag3"],
-        price: 100,
-        reviews: 100,
-        rating: 4,
-    },
-];
+export interface ClubImageType {
+    id: string;
+    imageUrl: string;
+}
+
+interface ClubType {
+    id: string;
+    clubName: string;
+    clubAddress: string;
+    clubPhone: string;
+    clubDescription: string;
+    openTime: string;
+    closeTime: string;
+    clubImages: ClubImageType[];
+    reviews: ReviewType[];
+}
 
 function ClubsList() {
-    // const {data, isLoading, isError, error} = useGetClubsQuery("");
-    // useEffect(() => {
-    //     console.log(data);
-    //     console.log(isLoading);
-    //     console.log(isError);
-    //     console.log(error);
-    // }, [isLoading]);
+    const {data: clubs, isError} = useGetClubsQuery("");
+
+    if (isError || !clubs) {
+        return <div>Error</div>;
+    }
 
     return (
         <div className="w-full flex justify-center py-12">
             <div className="w-3/4">
                 <Filter />
                 <ul className="grid grid-cols-3 gap-8 mt-12">
-                    {clubs.map((club) => (
+                    {clubs.map((club: ClubType) => (
                         <li key={club.id} className="col-span-1">
                             <ClubCard {...club} />
                         </li>
