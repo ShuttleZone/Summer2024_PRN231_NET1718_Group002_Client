466b1872b479e27324d82fe094bc652294f73d95
duykasama <duykasama@gmail.com>
2024-06-07 04:22:14 +0700

    [Deployment][Duy] add integration and deployment pipeline

---
+++ .deploy/terraform/main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.30.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_compute_instance" "shuttle_zone_web_client" {
+  boot_disk {
+    auto_delete = true
+    device_name = "shuttle-zone-client"
+
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20240515"
+      size  = 10
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "e2-medium"
+  name         = "shuttle-zone-client"
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "projects/shuttle-zone-client/regions/asia-southeast1/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  zone = var.zone
+}
