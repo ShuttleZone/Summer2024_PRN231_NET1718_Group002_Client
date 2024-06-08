e7ade7291d000233158746c5b76ea8ca4995d48b
duykasama <duykasama@gmail.com>
2024-06-08 11:22:37 +0700

    [Toast][Duy] show message from api after making a reservation, redirect if book successfully

--- src/layouts/UserLayout/index.tsx
+++ src/layouts/UserLayout/index.tsx
@@ -2,17 +2,23 @@ import {Outlet} from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import Spinner from "@/components/Spinner";
+import {Toaster} from "@/components/ui/toaster";
 
 function UserLayout() {
     return (
-        <div className="h-screen flex flex-col">
-            <Header />
-            <main className="flex-grow h-fit">
-                <Hero />
-                <Outlet />
-            </main>
-            <Footer />
-        </div>
+        <>
+            <div className="h-screen flex flex-col relative">
+                <Header />
+                <main className="flex-grow h-fit">
+                    <Hero />
+                    <Outlet />
+                </main>
+                <Footer />
+            </div>
+            <Spinner />
+            <Toaster />
+        </>
     );
 }
 
