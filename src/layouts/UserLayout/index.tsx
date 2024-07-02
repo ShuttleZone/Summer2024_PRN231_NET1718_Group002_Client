24b11c740d43e526135d7dc2b2f56f9b1471b891
supernhilty <nhilty302814@gmail.com>
2024-07-02 09:34:13 +0700

    [Refactor][Nhi] chore: Remove Hero component from UserLayout

--- src/layouts/UserLayout/index.tsx
+++ src/layouts/UserLayout/index.tsx
@@ -1,7 +1,6 @@
 import {Outlet} from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Hero from "./components/Hero";
 import Spinner from "@/components/Spinner";
 import {Toaster} from "@/components/ui/toaster";
 
