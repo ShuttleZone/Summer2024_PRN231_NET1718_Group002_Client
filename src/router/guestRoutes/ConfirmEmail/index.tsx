62f6643f62053bed8b842648c1cecbb1aaa788fa
HaiHungNguyenn <hainhse173100@fpt.edu.vn>
2024-07-05 00:38:47 +0700

    [Resolve][Hai] Move last two commits to new branch

--- src/router/guestRoutes/ConfirmEmail/index.tsx
+++ src/router/guestRoutes/ConfirmEmail/index.tsx
@@ -5,26 +5,23 @@ function ConfirmEmail() {
     const {email} = location.state || {};
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 p-6">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100 p-6">
             <div className="bg-white rounded-3xl shadow-xl p-8 max-w-md w-full text-center">
                 <img
                     src="src/assets/images/email.jpg"
-                    alt="Email Confirmation"
+                    alt="Xác Nhận Email"
                     className="w-24 h-24 mx-auto mb-4"
                 />
                 <h2 className="text-4xl font-extrabold text-gray-900 mb-4">
-                    Email Confirmation
+                    Xác Nhận Email
                 </h2>
                 <p className="text-gray-700 mb-6">
-                    We have sent an email to{" "}
+                    Chúng tôi đã gửi một email đến{" "}
                     <span className="font-semibold text-gray-900">{email}</span>{" "}
-                    to confirm the validity of your email address. After
-                    receiving the email, follow the link provided to complete
-                    your registration.
+                    để xác nhận tính hợp lệ của địa chỉ email của bạn. Sau khi
+                    nhận được email, hãy làm theo liên kết được cung cấp để hoàn
+                    tất việc đăng ký của bạn.
                 </p>
-                {/* <button className="w-full bg-pink-500 text-white py-3 rounded-full shadow-md hover:bg-pink-600 focus:outline-none focus:ring-4 focus:ring-pink-300">
-                    Resend Email
-                </button> */}
             </div>
         </div>
     );
