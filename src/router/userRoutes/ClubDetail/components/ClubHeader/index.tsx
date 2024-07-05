ece11285d4da26f4a50a50f45ec77af12bc5a4b1
duykasama <duykasama@gmail.com>
2024-07-05 21:30:59 +0700

    [Vietnamese][Duy] club detail

--- src/router/userRoutes/ClubDetail/components/ClubHeader/index.tsx
+++ src/router/userRoutes/ClubDetail/components/ClubHeader/index.tsx
@@ -12,7 +12,7 @@ interface ClubHeaderProps {
 
 function ClubHeader({name, address, phone, reviews}: ClubHeaderProps) {
     return (
-        <div className="flex justify-between items-end border border-black/10 rounded p-4">
+        <div className="flex justify-between items-end border border-black/10 rounded p-4 gap-12">
             <div className="flex flex-col gap-3">
                 <h1 className="text-[2rem] font-bold">{name}</h1>
                 <div className="flex items-center gap-4 text-sm opacity-60">
@@ -29,19 +29,19 @@ function ClubHeader({name, address, phone, reviews}: ClubHeaderProps) {
             <div className="flex justify-between items-center gap-6">
                 <Link
                     to={"court-booking"}
-                    className="flex justify-between items-center gap-2 border border-black/10 rounded px-2 py-1 bg-green-300"
+                    className="flex justify-between items-center gap-2 border border-black/10 rounded px-12 text-nowrap py-1 bg-green-300"
                 >
-                    <span className="px-12">Book</span>
+                    Đặt sân
                 </Link>
-                <button className="flex justify-between items-center gap-2 border border-black/10 rounded px-2 py-1">
+                <button className="flex justify-between items-center gap-2 border border-black/10 rounded px-2 py-1 text-nowrap">
                     <CiShare2 />
-                    <span>Share</span>
+                    <span>Chia sẻ</span>
                 </button>
                 <div className="flex gap-4 items-center">
                     <span className="rounded-full p-2 bg-green-600 text-white text-xl font-semibold">
                         4.0
                     </span>
-                    <div>
+                    <div className="text-nowrap">
                         <ul className="flex">
                             {Array.from({length: reviews}).map((_, index) => (
                                 <li key={index}>
@@ -49,7 +49,7 @@ function ClubHeader({name, address, phone, reviews}: ClubHeaderProps) {
                                 </li>
                             ))}
                         </ul>
-                        <span>15 Reviews</span>
+                        <span>15 đánh giá</span>
                     </div>
                 </div>
             </div>
