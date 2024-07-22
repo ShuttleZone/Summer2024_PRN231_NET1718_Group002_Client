31536b3674b087b383503f4b3fb2afd4f14b0c6c
duykasama <duykasama@gmail.com>
2024-07-22 23:32:13 +0700

    [Fix][Duy] remove checkboxes on login and register page

--- src/router/guestRoutes/Register/RegisterForm/index.tsx
+++ src/router/guestRoutes/Register/RegisterForm/index.tsx
@@ -293,7 +293,7 @@ const RegisterForm = () => {
                                     required
                                 />
                             </div>
-                            <div className="flex items-start mb-5">
+                            {/* <div className="flex items-start mb-5">
                                 <div className="flex items-center h-5">
                                     <input
                                         value={formData.repassword}
@@ -313,7 +313,7 @@ const RegisterForm = () => {
                                         điều khoản dịch vụ
                                     </a>
                                 </label>
-                            </div>
+                            </div> */}
                             <button
                                 type="submit"
                                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
