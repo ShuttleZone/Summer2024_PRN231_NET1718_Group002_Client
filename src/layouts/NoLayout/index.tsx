55314537b88f6ee570bae48e6a03f371ae10b3ad
supernhilty <nhilty302814@gmail.com>
2024-07-23 22:32:41 +0700

    [Fix][Nhi] fix: show toast on payment

--- src/layouts/NoLayout/index.tsx
+++ src/layouts/NoLayout/index.tsx
@@ -1,7 +1,13 @@
 import {Outlet} from "react-router-dom";
+import {Toaster} from "@/components/ui/toaster";
 
 function NoLayout() {
-    return <Outlet />;
+    return (
+        <>
+            <Outlet />
+            <Toaster />
+        </>
+    );
 }
 
 export default NoLayout;
